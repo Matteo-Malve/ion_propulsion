23fde29c740b620ba01426e07b4fd73f9ef5f484
Matteo-Malve <126158842+Matteo-Malve@users.noreply.github.com>
2023-05-25 11:25:48 +0200

    C'è evidentemente da sistemare assemble_rhs

--- src/Foundamentals/DualFunctional.h
+++ src/Foundamentals/DualFunctional.h
@@ -101,10 +101,10 @@ EmitterFlux<dim>::assemble_rhs(const DoFHandler<dim> &dof_handler,
     QGauss<dim>          quadrature(dof_handler.get_fe().degree + 1);
     QGauss<dim-1>        face_quadrature(dof_handler.get_fe().degree + 1);
     
-    FEValues<dim>        fe_values(dof_handler.get_fe(),
+    /* FEValues<dim>        fe_values(dof_handler.get_fe(),
                                  quadrature,
                                  update_gradients | update_quadrature_points |
-                                 update_JxW_values);  
+                                 update_JxW_values);  */
 
     FEFaceValues<dim>    fe_face_values(dof_handler.get_fe(),
                                         face_quadrature,
@@ -116,9 +116,9 @@ EmitterFlux<dim>::assemble_rhs(const DoFHandler<dim> &dof_handler,
     const unsigned int dofs_per_face = dof_handler.get_fe().n_dofs_per_face();
     const unsigned int dofs_per_cell = dof_handler.get_fe().n_dofs_per_cell();
 
-    Vector<double>     cell_rhs_face(dofs_per_face);
+    //Vector<double>     cell_rhs_face(dofs_per_face);
     Vector<double>     cell_rhs(dofs_per_cell);
-    std::vector<unsigned int> local_dof_face_indices(dofs_per_face);
+    //std::vector<unsigned int> local_dof_face_indices(dofs_per_face);
     std::vector<unsigned int> local_dof_indices(dofs_per_cell);
 
     for (const auto &cell : dof_handler.active_cell_iterators())
@@ -127,18 +127,19 @@ EmitterFlux<dim>::assemble_rhs(const DoFHandler<dim> &dof_handler,
             const Point<dim> c = face->center();
             if ((c[1] < 0.2) && (c[0] <= wire_radius) && (c[0] >= -wire_radius))
 
-                cell_rhs = 0;
+                cell_rhs = 0;   // ?    Vettore = 0 ?
+                                // Perchè mettiamo a zero in questo rettangolo?
             for (unsigned int q = 0; q < n_face_q_points; ++q) {
                 auto n = fe_face_values.normal_vector(q);
                 for (unsigned int i = 0; i < dofs_per_face; ++i) {
                     for (unsigned int k = 0; k < dim; ++k)
                         cell_rhs(i) += fe_face_values.shape_grad(i, q)[k] * n[k];
-                    cell_rhs(i) = cell_rhs(i) * fe_face_values.JxW(q);
+                    cell_rhs(i) *= fe_face_values.JxW(q);  // ??    access operator [], perchè () ?
                 }
             }
             face->get_dof_indices(local_dof_indices);
             for (unsigned int i = 0; i < dofs_per_face; ++i)
-                rhs(local_dof_indices[i]) += cell_rhs(i);
+                rhs(local_dof_indices[i]) += cell_rhs(i);   // ??  idem sopra
         }
 
     //AssertThrow(false, ExcEvaluationPointNotFound(evaluation_point));
