98e12524f1498691c6ace8bcb6581abc88410c56
Matteo-Malve <126158842+Matteo-Malve@users.noreply.github.com>
2023-07-10 16:09:10 +0200

    modifica assemble_rhs() ma non va comunque

--- src/Foundamentals/DualFunctional.h
+++ src/Foundamentals/DualFunctional.h
@@ -96,58 +96,55 @@ void
 EmitterFlux<dim>::assemble_rhs(const DoFHandler<dim> &dof_handler,
                                Vector<double> &       rhs) const
 {
-    double wire_radius = 0.025;     // Terribile, da generalizzare
-    rhs.reinit(dof_handler.n_dofs());
-    QGauss<dim>          quadrature(dof_handler.get_fe().degree + 1);
-    QGauss<dim-1>        face_quadrature(dof_handler.get_fe().degree + 1);
-    
-    FEValues<dim>        fe_values(dof_handler.get_fe(),
-                                 quadrature,
-                                 update_gradients | update_quadrature_points |
-                                 update_JxW_values);  
+        double radius = 0.025;     // Terribile, da generalizzare
+        const Point<2> mesh_center(0, 0);
+        rhs.reinit(dof_handler.n_dofs());
+        QGauss<dim>          quadrature(dof_handler.get_fe().degree + 1);
+        QGauss<dim-1>        face_quadrature(dof_handler.get_fe().degree + 1);
 
-    FEFaceValues<dim>    fe_face_values(dof_handler.get_fe(),
-                                        face_quadrature,
-                                        update_gradients | update_quadrature_points |
-                                        update_JxW_values | update_normal_vectors);
+        FEValues<dim>        fe_values(dof_handler.get_fe(),
+                                       quadrature,
+                                       update_gradients | update_quadrature_points |
+                                       update_JxW_values);
 
-    const unsigned int n_face_q_points = face_quadrature.size();
-    // const unsigned int n_q_points      = quadrature.size();
-    const unsigned int dofs_per_face = dof_handler.get_fe().n_dofs_per_face();
-    //const unsigned int dofs_per_cell = dof_handler.get_fe().n_dofs_per_cell();
+        FEFaceValues<dim>    fe_face_values(dof_handler.get_fe(),
+                                            face_quadrature,
+                                            update_gradients | update_quadrature_points |
+                                            update_JxW_values | update_normal_vectors);
 
-    //Vector<double>     cell_rhs_face(dofs_per_face);
-    Vector<double>     cell_rhs(dofs_per_face);
-    std::vector<unsigned int> local_dof_face_indices(dofs_per_face);
-    //std::vector<unsigned int> local_dof_indices(dofs_per_cell);
+        const unsigned int n_face_q_points = face_quadrature.size();
+        const unsigned int dofs_per_face = dof_handler.get_fe().n_dofs_per_face();
 
-    for (const auto &cell : dof_handler.active_cell_iterators()){
-        fe_values.reinit(cell);
-        for (const auto &face : cell->face_iterators())
-          if (face->at_boundary()){
-            const Point<dim> c = face->center();
-            if ((c[1] < 0.2) && (c[0] <= wire_radius) && (c[0] >= -wire_radius)){
-                fe_face_values.reinit(cell, face);
-                cell_rhs = 0;   // ?    Vettore = 0 ?
-                                // Perchè mettiamo a zero in questo rettangolo?
-            for (unsigned int q = 0; q < n_face_q_points; ++q) {
-                auto n = fe_face_values.normal_vector(q);
-                for (unsigned int i = 0; i < dofs_per_face; ++i) {
-                    for (unsigned int k = 0; k < dim; ++k)
-                        cell_rhs[i] += fe_face_values.shape_grad(i, q)[k] * (-n[k]);
-                    cell_rhs[i] *= fe_face_values.JxW(q);  // ??    access operator [], perchè () ?
+        Vector<double>     face_rhs(dofs_per_face);
+        std::vector<unsigned int> local_dof_face_indices(dofs_per_face);
+
+        for (const auto &cell : dof_handler.active_cell_iterators()){
+            fe_values.reinit(cell);
+            for (const auto &face : cell->face_iterators())
+                if (face->at_boundary()){
+                    const Point<dim> & face_center = face->center();
+                    if (mesh_center.distance(face_center)<1.00001*radius) {
+                        fe_face_values.reinit(cell, face);
+                        face_rhs = 0;
+
+                        for (unsigned int q = 0; q < n_face_q_points; ++q) {
+                            auto n = fe_face_values.normal_vector(q);
+                            for (unsigned int i = 0; i < dofs_per_face; ++i) {
+                                for (unsigned int k = 0; k < dim; ++k)
+                                    face_rhs[i] += fe_face_values.shape_grad(i, q)[k] * (-n[k]);
+                                face_rhs[i] *= fe_face_values.JxW(q);
+                            }
+                        }
+
+                        face->get_dof_indices(local_dof_face_indices);
+                        for (unsigned int i = 0; i < dofs_per_face; ++i)
+                            rhs(local_dof_face_indices[i]) += face_rhs[i];
+                    }
                 }
-            }
-          
-            face->get_dof_indices(local_dof_face_indices);
-            for (unsigned int i = 0; i < dofs_per_face; ++i)
-                rhs(local_dof_face_indices[i]) += cell_rhs[i];   // ??  idem sopra
         }
-    }
-    }
 
-    //AssertThrow(false, ExcEvaluationPointNotFound(evaluation_point));
+        //AssertThrow(false, ExcEvaluationPointNotFound(evaluation_point));
 
-}   
+    }
 
 #endif //GETPOT_DUALFUNCTIONAL_H
