3a36f2f47837b83223619bc704b705faee8e2675
Matteo-Malve <126158842+Matteo-Malve@users.noreply.github.com>
2023-08-23 17:17:41 +0200

    External selection of grid to be loaded

--- src/Foundamentals/Framework.h
+++ src/Foundamentals/Framework.h
@@ -11,56 +11,105 @@
 template <int dim>
 class ErrorController;
 
-//static GetPot redefined_2_datafile("../../data_setup");
 
-template <int dim>
-struct Framework
-{
-public:
-    struct ProblemDescription {
-        unsigned int primal_fe_degree;
-        unsigned int dual_fe_degree;
-        std::unique_ptr<const DualFunctionalBase<dim>> dual_functional;
-        unsigned int max_degrees_of_freedom;
-        Triangulation<dim> triangulation;
-        // Tripletta infame
-        Function<dim> &      rhs_function;        // Not sure
-        //const Function<dim> &      boundary_values;     // Not sure
-        // Constructor
-        ProblemDescription(Function<dim>&);
-    };
-
-    static void run(const ProblemDescription &descriptor);
+template<int dim>
+struct ProblemDescription {
+    unsigned int primal_fe_degree;
+    unsigned int dual_fe_degree;
+    std::unique_ptr<const DualFunctionalBase<dim>> dual_functional;
+    unsigned int max_degrees_of_freedom;
+    Triangulation<dim> triangulation;
+    // Tripletta infame
+    Function<dim> &      rhs_function;        // Not sure
+    //const Function<dim> &      boundary_values;     // Not sure
+    // Constructor
+    ProblemDescription(Function<dim>&);
 };
 
-template <int dim>
-Framework<dim>::ProblemDescription::ProblemDescription(Function<dim>& rhs_func)
+template<int dim>
+ProblemDescription<dim>::ProblemDescription(Function<dim>& rhs_func)
         : primal_fe_degree(1)
         , dual_fe_degree(2)
         , max_degrees_of_freedom(50000),
-          rhs_function(rhs_func)
+          rhs_function(rhs_func) {}
 
-{}
 
-template <int dim>
-void Framework<dim>::run(const ProblemDescription &descriptor)
+template <int dim,unsigned int grid_option>
+struct Framework{
+public:
+    static void run(const ProblemDescription<dim> &descriptor);
+};
+
+template <int dim, unsigned int grid_option>
+void Framework<dim,grid_option>::run(const ProblemDescription<dim> &descriptor)
+
 {
-    //unsigned int grid_option=redefined_2_datafile("Load/GridOption",3);
-
     // MESH
     Triangulation<dim> triangulation(Triangulation<dim>::smoothing_on_refinement);
-    /*
-    if(grid_option==1)
-        CreateGrid<dim>(triangulation);
-    else if(grid_option==2)
-        LoadSecondGrid<dim>(triangulation);
-    else {
-        cout << "Error in reading Datafile!\n";
-        return;
+    switch(grid_option){
+        case 1:
+            CreateGrid<dim>(triangulation);
+        case 2:
+            LoadSecondGrid<dim>(triangulation);
+        default:
+            cout<<"ERROR2: Invalid grid_option id";
     }
-     */
-    //CreateGrid<dim>(triangulation);
-    LoadSecondGrid<dim>(triangulation);
+
+
+
+    // FE
+    const FE_Q<dim>       primal_fe(descriptor.primal_fe_degree);
+    const FE_Q<dim>       dual_fe(descriptor.dual_fe_degree);
+    // QUADRATURE
+    const QGauss<dim>     quadrature(descriptor.dual_fe_degree + 1);
+    const QGauss<dim - 1> face_quadrature(descriptor.dual_fe_degree + 1);
+
+    // SOLVER = pointer to BASE
+    std::unique_ptr<Base<dim>> solver;
+    solver = std::make_unique<ErrorController<dim>>(
+            triangulation,
+            primal_fe,
+            dual_fe,
+            quadrature,
+            face_quadrature,
+            descriptor.rhs_function,    // Not sure
+            //descriptor.boundary_values, // Not sure
+            *descriptor.dual_functional);
+
+
+    // REFINEMENT LOOP
+    for (unsigned int step = 0; true; ++step)
+    {
+        std::cout << "[Framework]Refinement cycle: " << step << std::endl;
+
+        solver->set_refinement_cycle(step);
+        solver->solve_problem();    // <--- Problema qui
+        solver->output_solution();
+
+
+        std::cout << "   [Framework]Number of degrees of freedom=" << solver->n_dofs()
+                  << std::endl;
+
+        if (solver->n_dofs() < descriptor.max_degrees_of_freedom) {
+            cout << "   [Framework]Prepare call to refine grid" << endl;
+            solver->refine_grid();
+        }
+        else
+            break;
+    }
+
+    std::cout << std::endl;
+}
+
+/*
+// Specialization for grid:option=1
+template<int dim>
+void Framework<dim,1>::run(const ProblemDescription<dim> &descriptor)
+{
+    // MESH
+    Triangulation<dim> triangulation(Triangulation<dim>::smoothing_on_refinement);
+    CreateGrid<dim>(triangulation);
+    //LoadSecondGrid<dim>(triangulation);
     
     // FE
     const FE_Q<dim>       primal_fe(descriptor.primal_fe_degree);
@@ -106,5 +155,58 @@ void Framework<dim>::run(const ProblemDescription &descriptor)
     std::cout << std::endl;
 }
 
+// Specialization for grid_option=2
+template<int dim>
+void Framework<dim,2>::run(const ProblemDescription<dim> &descriptor)
+{
+    // MESH
+    Triangulation<dim> triangulation(Triangulation<dim>::smoothing_on_refinement);
+    //CreateGrid<dim>(triangulation);
+    LoadSecondGrid<dim>(triangulation);
+
+    // FE
+    const FE_Q<dim>       primal_fe(descriptor.primal_fe_degree);
+    const FE_Q<dim>       dual_fe(descriptor.dual_fe_degree);
+    // QUADRATURE
+    const QGauss<dim>     quadrature(descriptor.dual_fe_degree + 1);
+    const QGauss<dim - 1> face_quadrature(descriptor.dual_fe_degree + 1);
+
+    // SOLVER = pointer to BASE
+    std::unique_ptr<Base<dim>> solver;
+    solver = std::make_unique<ErrorController<dim>>(
+            triangulation,
+            primal_fe,
+            dual_fe,
+            quadrature,
+            face_quadrature,
+            descriptor.rhs_function,    // Not sure
+            //descriptor.boundary_values, // Not sure
+            *descriptor.dual_functional);
+
+
+    // REFINEMENT LOOP
+    for (unsigned int step = 0; true; ++step)
+    {
+        std::cout << "[Framework]Refinement cycle: " << step << std::endl;
+
+        solver->set_refinement_cycle(step);
+        solver->solve_problem();    // <--- Problema qui
+        solver->output_solution();
+
+
+        std::cout << "   [Framework]Number of degrees of freedom=" << solver->n_dofs()
+                  << std::endl;
+
+        if (solver->n_dofs() < descriptor.max_degrees_of_freedom) {
+            cout << "   [Framework]Prepare call to refine grid" << endl;
+            solver->refine_grid();
+        }
+        else
+            break;
+    }
+
+    std::cout << std::endl;
+}
+*/
 
 #endif //GETPOT_FRAMEWORK_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Foundamentals/Framework.h"
 
+static GetPot redefined_2_datafile("../data_setup");
+unsigned int grid_option = redefined_2_datafile("Load/grid_option",3);
 
 int main()
 {
@@ -8,17 +10,23 @@ int main()
     {
         // Fix dim
         const unsigned int dim = 2;
-
-        // Define DESCRIPTOR
-        Functions::ZeroFunction<dim> zero_function;
-        Framework<dim>::ProblemDescription descriptor(zero_function);
-        // Fill DESCRIPTOR fields
-        descriptor.primal_fe_degree = 1;
-        descriptor.dual_fe_degree   = 2;
-        descriptor.dual_functional = std::make_unique<EmitterFlux<dim>>();
-        descriptor.max_degrees_of_freedom = 20000;
-        // RUN FRAMEWORK
-        Framework<dim>::run(descriptor);
+                // Define DESCRIPTOR
+                Functions::ZeroFunction <dim> zero_function;
+                ProblemDescription<dim> descriptor(zero_function);
+                // Fill DESCRIPTOR fields
+                descriptor.primal_fe_degree = 1;
+                descriptor.dual_fe_degree = 2;
+                descriptor.dual_functional = std::make_unique<EmitterFlux<dim>>();
+                descriptor.max_degrees_of_freedom = 20000;
+                // RUN FRAMEWORK
+        switch(grid_option) {
+            case 1:
+                Framework<dim, 1>::run(descriptor);
+            case 2:
+                Framework<dim, 2>::run(descriptor);
+            default:
+                cout<<"ERROR1: Invalid grid_option id";
+        }
     }
 
     catch (std::exception &exc)
