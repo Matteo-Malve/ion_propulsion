ca2a9bcb4c24cc554b531b067897b149a48d431d
Matteo-Malve <matteo.malvestiti@outlook.com>
2024-10-07 15:09:34 +0200

    minor changes to postprocessing file

--- postprocessing/paraview_ExactRgGradient_ProgrammableFilter.py
+++ postprocessing/paraview_ExactRgGradient_ProgrammableFilter.py
@@ -1,7 +1,7 @@
 
 # GradRg x,y components
 input0 = inputs[0]
-radiusDataArray = input0.PointData["radius"]
+radiusDataArray = input0.PointData["r"]
 xDataArray = input0.PointData["x"]
 yDataArray = input0.PointData["y"]
 numPoints = input0.GetNumberOfPoints()
@@ -12,16 +12,16 @@ R = 7.5e-4
 Ve = 20000.0
 for i in range(numPoints):
   if radiusDataArray[i] < 3*R:
-    RgGrad_x_Array[i] = xDataArray[i]/radiusDataArray[i]
-    dRgdr_Array[i] = Ve/R * ((radiusDataArray[i]-R)/(2*R)-1)
-    RgGrad_y_Array[i] = Ve/R * ((radiusDataArray[i]-R)/(2*R)-1) * yDataArray[i]/radiusDataArray[i]
+    dRgdr_Array[i] = - Ve/R * ((radiusDataArray[i]-R)/(2*R)-1)
+    RgGrad_x_Array[i] = dRgdr_Array[i] * xDataArray[i]/radiusDataArray[i]
+    RgGrad_y_Array[i] = dRgdr_Array[i] * yDataArray[i]/radiusDataArray[i]
   else:
     RgGrad_x_Array[i] = 0.0
     RgGrad_y_Array[i] = 0.0
     dRgdr_Array[i] = 0.0
-output.PointData.append(RgGrad_x_Array, "RgGrad_x")
-output.PointData.append(RgGrad_y_Array, "RgGrad_y")
-output.PointData.append(dRgdr_Array, "dRgdr")
+output.PointData.append(RgGrad_x_Array, "minus_RgGrad_x")
+output.PointData.append(RgGrad_y_Array, "minus_RgGrad_y")
+output.PointData.append(dRgdr_Array, "minus_dRgdr")
 
 # Then put components together in a vector with Calculator
 
@@ -36,6 +36,20 @@ alpha_Array=numpy.zeros(numPoints)
 for i in range(numPoints):
   alpha_Array[i] = numpy.arctan2(yDataArray[i], xDataArray[i]) # importante arctan2
 output.PointData.append(alpha_Array, "alpha")
+
+# Calcolo minus_grad_uh0 in cordinate cilindriche
+input0 = inputs[0]
+xDataArray = input0.PointData["x"]
+yDataArray = input0.PointData["y"]
+EDataArray = input0.PointData["minus_grad_uh0"]
+numPoints = input0.GetNumberOfPoints()
+E_r_array=numpy.zeros(numPoints)
+E_theta_array=numpy.zeros(numPoints)
+for i in range(numPoints):
+  E_r_array[i] = EDataArray[i][0]*numpy.cos(numpy.arctan2(yDataArray[i], xDataArray[i])) + EDataArray[i][1]*numpy.sin(numpy.arctan2(yDataArray[i], xDataArray[i]))
+  E_theta_array[i] = - EDataArray[i][0]*numpy.sin(numpy.arctan2(yDataArray[i], xDataArray[i])) + EDataArray[i][1]*numpy.cos(numpy.arctan2(yDataArray[i], xDataArray[i]))
+output.PointData.append(E_r_array, "minus_grad_uh0_r")
+output.PointData.append(E_theta_array, "minus_grad_uh0_theta")
 
 # Calcolo E in cordinate cilindriche
 input0 = inputs[0]
