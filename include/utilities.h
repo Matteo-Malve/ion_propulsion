1d23ec932259967cdce8510458edece16e4795e2
Matteo-Malve <matteo.malvestiti@outlook.com>
2024-10-29 19:18:19 +0100

    ErrataCorrige (forgot -eps0*epsr)  gaussian+sinusoid function and laplacian

--- include/utilities.h
+++ include/utilities.h
@@ -10,7 +10,7 @@ constexpr double pi = 3.14159265358979323846;
 std::string extract_mesh_name();
 
 // -----------------------------------------
-// RHS
+// 0
 // -----------------------------------------
 
 
@@ -24,6 +24,10 @@ public:
 			values[p] = 0.;
 	}
 };
+
+// -----------------------------------------
+// 1 - broken
+// -----------------------------------------
 
 template <int dim>
 class RightHandSide1 : public Function<dim>{
@@ -44,6 +48,30 @@ public:
 			return 0.;
 	}
 };
+
+template <int dim>
+class ExactSolution1 : public Function<dim>{
+public:
+	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
+		(void)component;
+		double k = 5.0;
+		const auto x = p[0];
+		const auto y = p[1];
+		if(std::abs(x) <= k*L && y<= k*L)
+			return Ve * (1 - 
+						 std::sin(std::abs(x)/L*pi) * 
+						 std::sin(y/L*pi) );
+						 //(1.-1./((k-1)*L)*(std::abs(x)-L)) *
+						 //(1.-1./((k-1)*L)*(y-L));
+		else
+			return 0.;
+	}
+};
+
+// -----------------------------------------
+// 2 - broken
+// -----------------------------------------
+
 template <int dim>
 class RightHandSide2 : public Function<dim>{
 public:
@@ -57,6 +85,22 @@ public:
 };
 
 template <int dim>
+class ExactSolution2 : public Function<dim>{
+public:
+	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
+		(void)component;
+		const auto x = p[0];
+		const auto y = p[1];
+		const auto r2 = x*x + y*y;
+		return exp(-r2/0.004);
+	}
+};
+
+// -----------------------------------------
+// 3 - broken
+// -----------------------------------------
+
+template <int dim>
 class RightHandSide3 : public Function<dim>{
 public:
 	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
@@ -64,6 +108,37 @@ public:
 		const auto x = p[0];
 		const auto y = p[1];
 		return eps_0 * eps_r *2. / (pow(0.004,4)) * (x*x + y*y);
+	}
+};
+
+template <int dim>
+class ExactSolution3 : public Function<dim>{
+public:
+	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
+		(void)component;
+		const auto x = p[0];
+		const auto y = p[1];
+		return 1. - x*x * y*y / pow(0.004,4);
+	}
+};
+
+// -----------------------------------------
+// 4 - WORKS
+// -----------------------------------------
+
+template <int dim>
+class ExactSolution4 : public Function<dim>{
+public:
+	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
+		(void)component;
+		
+		const auto x = p[0];
+		const auto y = p[1];
+		double r2 = x*x + y*y;
+    if(r2 <= Rc*Rc)
+      return AC*(pow((r2-R*R),3)) + AD*(pow((r2-R*R),2)) + AE*(r2-R*R) + AF;
+    else
+      return 0.;
 	}
 };
 
@@ -85,66 +160,79 @@ public:
 };
 
 // -----------------------------------------
-// Exact solution
+// 5 - 
 // -----------------------------------------
 
 
 template <int dim>
-class ExactSolution1 : public Function<dim>{
-public:
-	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
-		(void)component;
-		double k = 5.0;
-		const auto x = p[0];
-		const auto y = p[1];
-		if(std::abs(x) <= k*L && y<= k*L)
-			return Ve * (1 - 
-						 std::sin(std::abs(x)/L*pi) * 
-						 std::sin(y/L*pi) );
-						 //(1.-1./((k-1)*L)*(std::abs(x)-L)) *
-						 //(1.-1./((k-1)*L)*(y-L));
-		else
-			return 0.;
-	}
-};
-
-template <int dim>
-class ExactSolution2 : public Function<dim>{
-public:
-	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
-		(void)component;
-		const auto x = p[0];
-		const auto y = p[1];
-		const auto r2 = x*x + y*y;
-		return exp(-r2/0.004);
-	}
-};
-
-template <int dim>
-class ExactSolution3 : public Function<dim>{
-public:
-	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
-		(void)component;
-		const auto x = p[0];
-		const auto y = p[1];
-		return 1. - x*x * y*y / pow(0.004,4);
-	}
-};
-
-template <int dim>
-class ExactSolution4 : public Function<dim>{
+class ExactSolution5 : public Function<dim>{
 public:
 	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
 		(void)component;
 		
+		double sigma2 = 0.0000005;
+
 		const auto x = p[0];
 		const auto y = p[1];
-		double r2 = x*x + y*y;
-    if(r2 <= Rc*Rc)
-      return AC*(pow((r2-R*R),3)) + AD*(pow((r2-R*R),2)) + AE*(r2-R*R) + AF;
-    else
-      return 0.;
+		double r = sqrt(x*x + y*y);
+    
+		return exp(-(r-R)*(r-R)/(2.*sigma2))
+					 * Ve
+					 * (1. - sin(x/L*pi) * sin(y/L*pi));
+      
 	}
 };
+
+template <int dim>
+class RightHandSide5 : public Function<dim>{
+public:
+	virtual double value(const Point<dim>  &p, const unsigned int component = 0) const override{
+		(void)component;
+
+		double sigma2 = 0.0000005;
+
+		const auto x = p[0];
+		const auto y = p[1];
+		double r = sqrt(x*x + y*y);
+		double argX = pi * x / L;
+    double argY = pi * y / L;
+
+		// Break down the formula for readability
+    double expTerm = std::exp(-std::pow(R - r, 2) / (2 * sigma2));
+  
+    double sinArgX = std::sin(argX);
+    double sinArgY = std::sin(argY);
+    double cosArgX = std::cos(argX);
+    double cosArgY = std::cos(argY);
+    
+    double term1 = L * L * R * R * r;
+    double term2 = -L * L * R * R * r * sinArgX * sinArgY;
+    double term3 = -L * L * R * sigma2 * sinArgX * sinArgY;
+    double term4 = L * L * R * sigma2;
+    double term5 = 2 * L * L * R * x * x * sinArgX * sinArgY;
+    double term6 = -2 * L * L * R * x * x;
+    double term7 = 2 * L * L * R * y * y * sinArgX * sinArgY;
+    double term8 = -2 * L * L * R * y * y;
+    double term9 = -2 * L * L * sigma2 * r;
+    double term10 = 2 * L * L * sigma2 * r * sinArgX * sinArgY;
+    double term11 = L * L * x * x * r;
+    double term12 = L * L * y * y * r;
+    double term13 = -L * L * x * x * r * sinArgX * sinArgY;
+    double term14 = -L * L * y * y * r * sinArgX * sinArgY;
+    double term15 = 2 * pi * L * sigma2 * y * sinArgX * cosArgY * (r - R);
+    double term16 = 2 * pi * L * sigma2 * x * cosArgX * sinArgY * (r - R);
+    double term17 = 2 * pi * pi * sigma2 * sigma2 * r * sinArgX * sinArgY;
+    
+    double numerator = expTerm * (term1 + term2 + term3 + term4 + term5 + term6 + term7 + term8 +
+                                  term9 + term10 + term11 + term12 + term13 + term14 + term15 +
+                                  term16 + term17);
+    double denominator = L * L * sigma2 * sigma2 * r;
+    double laplacian = numerator / denominator;
+    return - eps_0 * eps_r * laplacian;
+	}
+};
+
+
+
 
 #endif
