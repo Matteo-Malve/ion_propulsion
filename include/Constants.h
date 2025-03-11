9bca3b448fe37a91d8b459b22e278a3a85d93fbe
Matteo-Malve <matteo.malvestiti@outlook.com>
2025-03-11 13:26:55 +0100

    Added GNU Licensing: COPYING and notices in every file

--- include/Constants.h
+++ include/Constants.h
@@ -1,3 +1,52 @@
+/* ------------------------------------------------------------------------
+ *
+ * SPDX-License-Identifier: GPL-3-or-later
+ * Copyright (C) 2023 - 2024 by Matteo Malvestiti
+ *
+ * This file is part of ion_propulsion.
+ *
+ * ion_propulsion is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * ion_propulsion is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ion_propulsion; see the file COPYING.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Author: Matteo Malvestiti, Politecnico di Milano, 2024
+ *
+ */
+
+
+////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2012-2025 The Octave Project Developers
+//
+//
+// This file is part of Octave.
+//
+// Octave is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Octave is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Octave; see the file COPYING.  If not, see
+// <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////
+
 #ifndef DEAL_II_NOT_IMPLEMENTED
 #define DEAL_II_NOT_IMPLEMENTED()              \
 do {                                         \
--- src/LaplaceSolver.cpp
+++ src/LaplaceSolver.cpp
@@ -1,5 +1,29 @@
-#include "LaplaceSolver.h"
+/* ------------------------------------------------------------------------
+*
+ * SPDX-License-Identifier: GPL-3-or-later
+ * Copyright (C) 2023 - 2024 by Matteo Malvestiti
+ *
+ * This file is part of ion_propulsion.
+ *
+ * ion_propulsion is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * ion_propulsion is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ion_propulsion; see the file COPYING.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Author: Matteo Malvestiti, Politecnico di Milano, 2024
+ *
+ */
 
+#include "LaplaceSolver.h"
 #include <Constants.h>
 
 namespace IonPropulsion{
